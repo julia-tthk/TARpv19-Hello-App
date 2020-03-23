9401ae2eed2222b8e1604c2b6c65c80cd9d1bd84
julia-vrn <julia.voronetskaja@gmail.com>
2020-03-23 22:28:43 +0200

    Ask User Name

--- TARpv19/Program.cs
+++ TARpv19/Program.cs
@@ -10,7 +10,11 @@ namespace TARpv19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Please, enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "!");
+
 
             Console.ReadLine();
         }
