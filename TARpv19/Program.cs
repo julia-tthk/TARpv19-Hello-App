913ed7768e31ace9146e24b560822d5d70348da5
julia-vrn <julia.voronetskaja@gmail.com>
2020-03-23 22:32:50 +0200

    Use String Interpolation

--- TARpv19/Program.cs
+++ TARpv19/Program.cs
@@ -13,9 +13,11 @@ namespace TARpv19
             Console.WriteLine("Please, enter your name: ");
             string name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name + "!");
+            //Console.WriteLine("Hello " + name + "!");
 
+            //string interpolation
 
+            Console.WriteLine($"Hello, {name}!");
             Console.ReadLine();
         }
     }
